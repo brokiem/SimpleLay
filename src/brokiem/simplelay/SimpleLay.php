0a0782b7463cfb77bff2b2b429ed9a8a429f7fc2
brokiem <61258338+brokiem@users.noreply.github.com>
2020-12-11 16:49:59 +0800

    Update SimpleLay.php

--- src/brokiem/simplelay/SimpleLay.php
+++ src/brokiem/simplelay/SimpleLay.php
@@ -34,19 +34,16 @@ class SimpleLay extends PluginBase implements Listener
 
 		switch (strtolower($command->getName())) {
 			case "lay":
-				if (isset($args[0])) {
-					if ($args[0] === "clear") {
-						if ($sender->hasPermission("clear.lay.entites")) {
-							foreach ($this->getServer()->getLevels() as $level) {
-								foreach ($level->getEntities() as $entities) {
-									if ($entities instanceof LayingEntity) {
-										$entities->flagForDespawn();
-									}
+				if (isset($args[0]) and $args[0] === "clear") {
+					if ($sender->hasPermission("clear.lay.entites")) {
+						foreach ($this->getServer()->getLevels() as $level) {
+							foreach ($level->getEntities() as $entities) {
+								if ($entities instanceof LayingEntity) {
+									$entities->flagForDespawn();
 								}
 							}
-
-							$sender->sendMessage(TextFormat::GOLD . "Lay entities cleared!");
 						}
+						$sender->sendMessage(TextFormat::GOLD . "Lay entities cleared!");
 					}
 				} else {
 					if ($this->isLaying($sender)) {
@@ -57,7 +54,6 @@ class SimpleLay extends PluginBase implements Listener
 				}
 				break;
 		}
-
 		return true;
 	}
 
@@ -69,27 +65,28 @@ class SimpleLay extends PluginBase implements Listener
 	private function setLay(Player $player)
 	{
 		$player->saveNBT();
-		
-		$nbt = Entity::createBaseNBT($player);
-		$nbt->setTag($player->namedtag->getTag("Skin"));
 
 		$pos = new Vector3($player->getX(), $player->getY() - 0.3, $player->getZ());
 
-		$layingEntity = new LayingEntity($player->getLevel(), $nbt);
+		$nbt = Entity::createBaseNBT($player, null, $player->getYaw(), $player->getPitch());
+		$nbt->setTag($player->namedtag->getTag("Skin"));
+
+		$layingEntity = new LayingEntity($player->getLevelNonNull(), $nbt, $player);
 		$layingEntity->getDataPropertyManager()->setBlockPos(LayingEntity::DATA_PLAYER_BED_POSITION, $pos);
-		$layingEntity->setGenericFlag(LayingEntity::DATA_FLAG_SLEEPING, true);
-		$layingEntity->setNameTag($player->getDisplayName());
 		$layingEntity->getDataPropertyManager()->setFloat(LayingEntity::DATA_BOUNDING_BOX_HEIGHT, 0.2);
-		$layingEntity->getArmorInventory()->setContents($player->getArmorInventory()->getContents());
-		$layingEntity->getInventory()->setContents($player->getInventory()->getContents());
-		$layingEntity->setSkin($player->getSkin());
-		$this->layingPlayer[$player->getId()] = $layingEntity;
+		$layingEntity->setGenericFlag(LayingEntity::DATA_FLAG_SLEEPING, true);
+
+		$layingEntity->setNameTag($player->getDisplayName());
 		$layingEntity->spawnToAll();
+
+		$this->layingPlayer[$player->getId()] = $layingEntity;
 
 		$player->setInvisible();
 		$player->setImmobile();
+		$player->setScale(0.01);
+
 		$player->teleport(new Vector3($player->getX(), $player->getY() - 1, $player->getZ()));
-		$player->setScale(0.1);
+
 		$player->sendMessage(TextFormat::GOLD . "You are now laying!");
 		$player->sendTip("Tap the sneak button to stand up");
 	}
@@ -135,9 +132,10 @@ class SimpleLay extends PluginBase implements Listener
 		$player->setInvisible(false);
 		$player->setImmobile(false);
 		$player->setScale(1);
-		$player->teleport(new Vector3($player->getX(), $player->getY() + 1, $player->getZ()));
-		$player->sendMessage(TextFormat::GOLD . "You are no longer laying.");
 
+		$player->teleport(new Vector3($player->getX(), $player->getY() + 1, $player->getZ()));
+
+		$player->sendMessage(TextFormat::GOLD . "You are no longer laying.");
 		unset($this->layingPlayer[$player->getId()]);
 
 		if ($entity instanceof LayingEntity) {
