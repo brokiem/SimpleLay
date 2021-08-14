4a434170ca11f51b3654d2d9cd27ba67dbed06df
brokiem <milyhqvideo@gmail.com>
2021-08-14 11:52:36 +0800

    add big copyright text

--- src/brokiem/simplelay/task/async/CheckUpdateTask.php
+++ src/brokiem/simplelay/task/async/CheckUpdateTask.php
@@ -2,6 +2,29 @@
 
 declare(strict_types=1);
 
+/*
+ *  ____    _                       _          _
+ * / ___|  (_)  _ __ ___    _ __   | |   ___  | |       __ _   _   _
+ * \___ \  | | | '_ ` _ \  | '_ \  | |  / _ \ | |      / _` | | | | |
+ *  ___) | | | | | | | | | | |_) | | | |  __/ | |___  | (_| | | |_| |
+ * |____/  |_| |_| |_| |_| | .__/  |_|  \___| |_____|  \__,_|  \__, |
+ *                         |_|                                 |___/
+ *
+ * Copyright (C) 2020 - 2021 brokiem
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ *
+ */
+
 namespace brokiem\simplelay\task\async;
 
 use brokiem\simplelay\SimpleLay;
